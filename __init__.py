1668ac18a457c1fbad87847c0c4ea6b74980d3cf
Idrees Aziz <khanidris.khan@gmail.com>
2025-07-17 20:10:49 +0300

    fixed all the spelling erros in engine name

--- __init__.py
+++ __init__.py
@@ -3,6 +3,6 @@ Swimlane Engine - A declarative video rendering engine using Blender's VSE
 Parses SWML (Swimlane Markup Language) files and generates videos
 """
 
-from .engine import SwimlanesEngine, SwmlError, main
+from .engine import SwimlaneEngine, SwmlError, main
 
 __version__ = "0.1.0"
